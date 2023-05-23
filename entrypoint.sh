e2ebc9674e6f8643a37f249ccead9e8c3c3e66fb
officialEmmel <emmel.offial@gmail.com>
2023-05-23 10:32:29 +0200

    fixed cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,11 @@ cronjob_log="${CRON_JOB_LOG:-/var/log/cron.log}"
 
 echo "$cronjob /usr/local/bin/cloudflare-ddns.sh >> $cronjob_log 2>&1" > /etc/cron.d/cloudflare-ddns
 
+chmod 0644 /etc/cron.d/cloudflare-ddns
+
+crontab /etc/cron.d/cloudflare-ddns
+
+touch $cronjob_log
+
 # start cron
 cron -f
